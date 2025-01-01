d4f6ffde8c1de628a4360317f5f8d0a5e8b674ee
1111211010 <144759442+1111211010@users.noreply.github.com>
2025-01-01 23:17:52 +0800

    Update views.py

--- mylineapp/views.py
+++ mylineapp/views.py
@@ -190,10 +190,6 @@ def callback(request):
                         event.reply_token,
                         TextSendMessage( text = replymsg ))
                     
-                     line_bot_api.reply_message(
-                        event.reply_token,
-                        ImageSendMessage(original_content_url='https://ws.taipei-101.com.tw/upload/firework/20220105/ada6f07c3d244c4f80e0f231be729313/ada6f07c3d244c4f80e0f231be729313.jpg',
-                        preview_image_url='https://ws.taipei-101.com.tw/upload/firework/20220105/ada6f07c3d244c4f80e0f231be729313/ada6f07c3d244c4f80e0f231be729313.jpg'))
                                                                                             
                      
 
