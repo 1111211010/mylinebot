7c5d6f5afaeb89402a825282007f38c3a133852b
1111211010 <144759442+1111211010@users.noreply.github.com>
2025-01-01 20:10:39 +0800

    Update views.py

--- mylineapp/views.py
+++ mylineapp/views.py
@@ -17,7 +17,7 @@ line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 def index(request):
-    return HttpResponse("阿母,我成功了~~!")
+    return HttpResponse("嗨嚕(❁´◡`❁)")
 
 # 擷取統一發票
 def invoice():
